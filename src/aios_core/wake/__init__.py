a537e6cf357cdc0fa916ba6de61c307876eb21a9
Haneof <131862491+Haneof@users.noreply.github.com>
2026-09-20 16:51:33 +0800

    core(c09): add wake package exports

---
+++ src/aios_core/wake/__init__.py
@@ -0,0 +1,19 @@
+"""Constitutional C09 wake scheduling and dispatch primitives."""
+
+from .service import (
+    Step0GateInput,
+    Step0GateResult,
+    WakeBus,
+    WakeSignalReceipt,
+    WakeSignalRequest,
+    WakeStateReceipt,
+)
+
+__all__ = [
+    "Step0GateInput",
+    "Step0GateResult",
+    "WakeBus",
+    "WakeSignalReceipt",
+    "WakeSignalRequest",
+    "WakeStateReceipt",
+]
