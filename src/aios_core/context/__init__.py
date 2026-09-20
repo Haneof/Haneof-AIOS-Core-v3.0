8a349f1f5152ed2074b5ecfdc970d202f2b73c82
Haneof <131862491+Haneof@users.noreply.github.com>
2026-09-20 16:13:07 +0800

    core(p14): export continuity contracts

--- src/aios_core/context/__init__.py
+++ src/aios_core/context/__init__.py
@@ -1,5 +1,23 @@
 """Model context preparation for AIOS v3.0."""
 
+from .continuity import (
+    ConversationContinuityService,
+    ConversationMessage,
+    ContinuitySnapshot,
+    ROUND_SUMMARY_KIND,
+    RoundSummaryCommit,
+    RoundSummaryRequest,
+)
 from .controller import ContextController, ModelContextBundle, estimate_tokens
 
-__all__ = ["ContextController", "ModelContextBundle", "estimate_tokens"]
+__all__ = [
+    "ContextController",
+    "ModelContextBundle",
+    "estimate_tokens",
+    "ConversationContinuityService",
+    "ConversationMessage",
+    "ContinuitySnapshot",
+    "ROUND_SUMMARY_KIND",
+    "RoundSummaryCommit",
+    "RoundSummaryRequest",
+]
